791cbac92d9e5be023446d543ad77377fbce65fd
EZheng23 <familiazheng23@gmail.com>
2026-05-01 15:09:05 -0500

    solucion hasta ejercicio 5

--- serializer.h
+++ serializer.h
@@ -1,9 +1,47 @@
 #ifndef PROG3_UNIT2_TEMPLATE_CLASES_V2026_01_SERIALIZER_H
 #define PROG3_UNIT2_TEMPLATE_CLASES_V2026_01_SERIALIZER_H
+#include <string>
+#include <sstream>
 
+template <typename... Ts>
+struct Serializer;
 
-class serializer {
+template <typename T>
+struct Serializer<T> {
+    static std::string to_string(const T& x) {
+        std::ostringstream oss;
+        oss << x;
+        return oss.str();
+    }
 };
 
+template <>
+struct Serializer<int> {
+    static std::string to_string(int x) {
+        return std::to_string(x);
+    }
+};
 
+template <>
+struct Serializer<double> {
+    static std::string to_string(double x) {
+        std::ostringstream oss;
+        oss << x;
+        return oss.str();
+    }
+};
+
+template <>
+struct Serializer<std::string> {
+    static std::string to_string(const std::string& x) {
+        return x;
+    }
+};
+
+template <typename T, typename U, typename... Rest>
+struct Serializer<T, U, Rest...> {
+    static std::string to_string(const T& first, const U& second, const Rest&... rest) {
+        return Serializer<T>::to_string(first) + "|" + Serializer<U, Rest...>::to_string(second, rest...);
+    }
+};
 #endif //PROG3_UNIT2_TEMPLATE_CLASES_V2026_01_SERIALIZER_H
